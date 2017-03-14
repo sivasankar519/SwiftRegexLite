e1f6d982b98bc765b5ddf8f6eeab6da7271c5608
sdevab001c <siva_devabathini@comcast.com>
2017-03-14 11:22:00 -0400

    commit

---
+++ SwiftRegexLite/SwiftRegexLite.h
@@ -0,0 +1,19 @@
+//
+//  SwiftRegexLite.h
+//  SwiftRegexLite
+//
+//  Created by Devabathini, Siva (Contractor) on 3/14/17.
+//  Copyright © 2017 --. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftRegexLite.
+FOUNDATION_EXPORT double SwiftRegexLiteVersionNumber;
+
+//! Project version string for SwiftRegexLite.
+FOUNDATION_EXPORT const unsigned char SwiftRegexLiteVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftRegexLite/PublicHeader.h>
+
+#import <Foundation/Foundation.h>
